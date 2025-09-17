f3bd509391c69bb7ab0c70c406a571bcdb037adc
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-09-17 07:14:23 +0300

    working on ch2 DogsDoors2

---
+++ RicksGuitars/RicksGuitars/Guitar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RicksGuitars
+{
+    public class Guitar
+    {
+		private string serialNumber;
+        private double price;
+        private GuitarSpec guitarSpec;
+
+        public Guitar(string serialNumber, double price, GuitarSpec guitarSpec)
+        {
+            SerialNumber = serialNumber;
+            Price = price;
+            GuitarSpec = guitarSpec;
+        }
+
+        public string SerialNumber { get => serialNumber; private set => serialNumber = value; }
+        public double Price { get => price; set => price = value; }
+        public GuitarSpec GuitarSpec { get => guitarSpec; private set => guitarSpec = value; }
+
+
+        
+        public override string ToString()
+        {
+            return $"Guitar: Serial Number: {SerialNumber},\n\tPrice: {Price}," +
+               $"\n\t{GuitarSpec.ToString()}";
+        }
+
+        
+    }
+}
---
+++ RicksGuitars/RicksGuitars/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RicksGuitars
+{
+    public class Inventory
+    {
+        private List<Guitar> guitarList;
+
+        public Inventory()
+        {
+            guitarList = new List<Guitar>();
+        }
+
+        public void AddGuitar(string serialNumber, double price, GuitarSpec guitarSpec)
+        {
+            Guitar guitar = new Guitar(serialNumber, price, guitarSpec);
+
+            guitarList.Add(guitar);
+        }
+
+        public Guitar GetGuitar(string serialNumber)
+        {
+            foreach (var guitar in guitarList)
+            {
+                if (guitar.SerialNumber == serialNumber) return guitar;
+            }
+
+            return null;
+        }
+
+        public List<Guitar> Search(GuitarSpec guitarSpec)
+        {
+            List<Guitar> List = new List<Guitar> ();
+
+            foreach (var guitar in guitarList)
+            {
+                if (guitarSpec.Equals(guitar.GuitarSpec))
+                {
+                    List.Add(guitar);
+                }
+            }
+            return List;
+        }
+    }
+}
